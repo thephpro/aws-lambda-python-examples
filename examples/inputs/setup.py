991e6c401f15e48dc6d4045dea0d2d7318712418
Mr Prime <151929300+thephpro@users.noreply.github.com>
2024-08-30 20:29:13 +0100

    Changes generated by AI

--- examples/inputs/setup.py
+++ examples/inputs/setup.py
@@ -1,7 +1,24 @@
+thon
 from setuptools import setup, find_packages
 
 setup(
     name="inputs",
+    version="0.1.0",  # Specify your package version
+    author="Your Name",  # Replace with your name
+    author_email="your.email@example.com",  # Replace with your email
+    description="A brief description of your package",  # Short description
+    long_description=open('README.md').read(),  # Read long description from README
+    long_description_content_type='text/markdown',  # Specify the markdown format
+    url="https://github.com/yourusername/inputs",  # Replace with your project URL
     packages=find_packages('src/inputs'),
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Specify minimum Python version
+    install_requires=[
+        # List your package dependencies here
+    ],
 )
